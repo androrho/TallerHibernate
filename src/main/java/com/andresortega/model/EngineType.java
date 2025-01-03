6fb1e46426d1a3abc047be79f1e4f7d5b0832ca9
Andrés <Andrs@LAPTOP-KVI0CD6M>
2025-01-03 17:05:26 +0100

    Enum de EngineType simplificado

--- src/main/java/com/andresortega/model/EngineType.java
+++ src/main/java/com/andresortega/model/EngineType.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.andresortega.model;
-
-import jakarta.persistence.AttributeConverter;
-import jakarta.persistence.Converter;
-import java.util.EnumSet;
 
 /**
  *
- * @author andres
+ * @author Andrés
  */
 public enum EngineType {
     /**
@@ -33,43 +25,4 @@ public enum EngineType {
      * numeric value of {@code 4}.
      */
     HYBRID;
-    /**
-     * Private cache of all the constants.
-     */
-    private static final EngineType[] ENUMS = EngineType.values();
-
-    public static EngineType of(int engineType) {
-        if (engineType < 1 || engineType > 5) {
-            throw new NullPointerException("Invalid value for EngineType: " + engineType);
-        }
-        return ENUMS[engineType - 1];
-    }
-
-    @Converter(autoApply = true)
-    public static class EnumSetConverter implements AttributeConverter<EnumSet<EngineType>, Integer> {
-
-        @Override
-        public Integer convertToDatabaseColumn(EnumSet<EngineType> enumSet) {
-            int encoded = 0;
-            var values = EngineType.values();
-            for (int i = 0; i < values.length; i++) {
-                if (enumSet.contains(values[i])) {
-                    encoded |= 1 << i;
-                }
-            }
-            return encoded;
-        }
-
-        @Override
-        public EnumSet<EngineType> convertToEntityAttribute(Integer encoded) {
-            var set = EnumSet.noneOf(EngineType.class);
-            var values = EngineType.values();
-            for (int i = 0; i < values.length; i++) {
-                if (((1 << i) & encoded) != 0) {
-                    set.add(values[i]);
-                }
-            }
-            return set;
-        }
-    }
 }
