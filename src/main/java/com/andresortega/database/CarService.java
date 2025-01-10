2728f75036a5e8f75fb524d35db58af656af3dc5
dam <dam@205P13>
2025-01-10 09:08:21 +0100

    cambiado de Id a dni y licensePlate

--- src/main/java/com/andresortega/database/CarService.java
+++ src/main/java/com/andresortega/database/CarService.java
@@ -9,113 +9,102 @@ import java.util.List;
  *
  * @author Andrés
  */
-
 public class CarService {
 
-
-
-public static void create(Car object) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    em.persist(object);
-    tx.commit();
-    em.close();
-}
-
-public static void update(Car object) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    em.merge(object);
-    tx.commit();
-    em.close();
-}
-
-public static Car getCar(Integer id) {
-    return load(id);
-}
-
-public static String getLicensePlate(Integer id) {
-    Car car = findById(id);
-    if (car != null) {
-        return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"car\",\n      \"licensePlate\": \"" + car.getLicensePlate() + "\",\n      \"query\":\"success\"\n   }\n]";
+    public static void create(Car object) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(object);
+        tx.commit();
+        em.close();
     }
-    return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"car\",\n      \"query\":\"failed\"\n   }\n]";
-}
 
-public static String getBrand(Integer id) {
-    Car car = findById(id);
-    if (car != null) {
-        return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"car\",\n      \"brand\": \"" + car.getBrand() + "\",\n      \"query\":\"success\"\n   }\n]";
+    public static void update(Car object) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.merge(object);
+        tx.commit();
+        em.close();
     }
-    return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"car\",\n      \"query\":\"failed\"\n   }\n]";
-}
 
-public static String getModel(Integer id) {
-    Car car = findById(id);
-    if (car != null) {
-        return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"car\",\n      \"model\": \"" + car.getModel() + "\",\n      \"query\":\"success\"\n   }\n]";
+    public static Car getCar(String licensePlate) {
+        return load(licensePlate);
     }
-    return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"car\",\n      \"query\":\"failed\"\n   }\n]";
-}
 
-public static String getEngineType(Integer id) {
-    Car car = findById(id);
-    if (car != null) {
-        return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"car\",\n      \"engineType\": \"" + car.getEngineType() + "\",\n      \"query\":\"success\"\n   }\n]";
+    public static String getBrand(String licensePlate) {
+        Car car = findByLicensePlate(licensePlate);
+        if (car != null) {
+            return "[\n   {\n      \"licensePlate\":" + licensePlate + ",\n      \"type\":\"car\",\n      \"brand\": \"" + car.getBrand() + "\",\n      \"query\":\"success\"\n   }\n]";
+        }
+        return "[\n   {\n      \"licensePlate\":" + licensePlate + ",\n      \"type\":\"car\",\n      \"query\":\"failed\"\n   }\n]";
     }
-    return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"car\",\n      \"query\":\"failed\"\n   }\n]";
-}
 
-public static Car findById(Integer id) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    Car object = em.find(Car.class, id);
-    tx.commit();
-    em.close();
-    return object;
-}
+    public static String getModel(String licensePlate) {
+        Car car = findByLicensePlate(licensePlate);
+        if (car != null) {
+            return "[\n   {\n      \"licensePlate\":" + licensePlate + ",\n      \"type\":\"car\",\n      \"model\": \"" + car.getModel() + "\",\n      \"query\":\"success\"\n   }\n]";
+        }
+        return "[\n   {\n      \"licensePlate\":" + licensePlate + ",\n      \"type\":\"car\",\n      \"query\":\"failed\"\n   }\n]";
+    }
 
-public static Car load(Integer id) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    Car objectResult = em.find(Car.class, id);
-    tx.commit();
-    em.close();
-    return objectResult;
-}
+    public static String getEngineType(String licensePlate) {
+        Car car = findByLicensePlate(licensePlate);
+        if (car != null) {
+            return "[\n   {\n      \"licensePlate\":" + licensePlate + ",\n      \"type\":\"car\",\n      \"engineType\": \"" + car.getEngineType() + "\",\n      \"query\":\"success\"\n   }\n]";
+        }
+        return "[\n   {\n      \"licensePlate\":" + licensePlate + ",\n      \"type\":\"car\",\n      \"query\":\"failed\"\n   }\n]";
+    }
 
-public static Car load(Car object) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    Car objectResult = em.find(Car.class, object.getCarId());
-    tx.commit();
-    em.close();
-    return objectResult;
-}
+    public static Car findByLicensePlate(String licensePlate) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Car object = em.find(Car.class, licensePlate);
+        tx.commit();
+        em.close();
+        return object;
+    }
 
-public static void deleteById(Integer id) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    em.remove(em.find(Car.class, id));
-    tx.commit();
-    em.close();
-}
+    public static Car load(String licensePlate) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Car objectResult = em.find(Car.class, licensePlate);
+        tx.commit();
+        em.close();
+        return objectResult;
+    }
 
-public static void delete(Car object) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    em.remove(em.find(Object.class, object.getCarId()));
-    tx.commit();
-    em.close();
-}
-/* ACTUALMENTE NO SIRVE PERO LO DEJO
+    public static Car load(Car object) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Car objectResult = em.find(Car.class, object.getLicensePlate());
+        tx.commit();
+        em.close();
+        return objectResult;
+    }
+
+    public static void deleteByLicensePlate(String licensePlate) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.remove(em.find(Car.class, licensePlate));
+        tx.commit();
+        em.close();
+    }
+
+    public static void delete(Car object) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.remove(em.find(Object.class, object.getLicensePlate()));
+        tx.commit();
+        em.close();
+    }
+    /* ACTUALMENTE NO SIRVE PERO LO DEJO
 public static List<Car> findByName(String name) {
     EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
     EntityTransaction tx = em.getTransaction();
@@ -125,5 +114,5 @@ public static List<Car> findByName(String name) {
     em.close();
     return list;
 }
-*/
+     */
 }
--- src/main/java/com/andresortega/database/CustomerService.java
+++ src/main/java/com/andresortega/database/CustomerService.java
@@ -9,100 +9,93 @@ import jakarta.persistence.EntityTransaction;
  * @author Andrés
  */
 public class CustomerService {
+
     public static void create(Customer object) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    em.persist(object);
-    tx.commit();
-    em.close();
-}
-
-public static void update(Customer object) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    em.merge(object);
-    tx.commit();
-    em.close();
-}
-
-public static Customer getCustomer(Integer id) {
-    return load(id);
-}
-
-public static String getDni(Integer id) {
-    Customer customer = findById(id);
-    if (customer != null) {
-        return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"customer\",\n      \"dni\": \"" + customer.getDni() + "\",\n      \"query\":\"success\"\n   }\n]";
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(object);
+        tx.commit();
+        em.close();
     }
-    return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"customer\",\n      \"query\":\"failed\"\n   }\n]";
-}
 
-public static String getName(Integer id) {
-    Customer customer = findById(id);
-    if (customer != null) {
-        return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"customer\",\n      \"name\": \"" + customer.getName() + "\",\n      \"query\":\"success\"\n   }\n]";
+    public static void update(Customer object) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.merge(object);
+        tx.commit();
+        em.close();
     }
-    return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"customer\",\n      \"query\":\"failed\"\n   }\n]";
-}
 
-public static String getAge(Integer id) {
-    Customer customer = findById(id);
-    if (customer != null) {
-        return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"customer\",\n      \"age\": \"" + customer.getAge() + "\",\n      \"query\":\"success\"\n   }\n]";
+    public static Customer getCustomer(String dni) {
+        return load(dni);
     }
-    return "[\n   {\n      \"id\":"+id+",\n      \"type\":\"customer\",\n      \"query\":\"failed\"\n   }\n]";
-}
 
-public static Customer findById(Integer id) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    Customer object = em.find(Customer.class, id);
-    tx.commit();
-    em.close();
-    return object;
-}
+    public static String getName(String dni) {
+        Customer customer = findByDni(dni);
+        if (customer != null) {
+            return "[\n   {\n      \"dni\":" + dni + ",\n      \"type\":\"customer\",\n      \"name\": \"" + customer.getName() + "\",\n      \"query\":\"success\"\n   }\n]";
+        }
+        return "[\n   {\n      \"dni\":" + dni + ",\n      \"type\":\"customer\",\n      \"query\":\"failed\"\n   }\n]";
+    }
 
-public static Customer load(Integer id) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    Customer objectResult = em.find(Customer.class, id);
-    tx.commit();
-    em.close();
-    return objectResult;
-}
+    public static String getAge(String dni) {
+        Customer customer = findByDni(dni);
+        if (customer != null) {
+            return "[\n   {\n      \"dni\":" + dni + ",\n      \"type\":\"customer\",\n      \"age\": \"" + customer.getAge() + "\",\n      \"query\":\"success\"\n   }\n]";
+        }
+        return "[\n   {\n      \"dni\":" + dni + ",\n      \"type\":\"customer\",\n      \"query\":\"failed\"\n   }\n]";
+    }
 
-public static Customer load(Customer object) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    Customer objectResult = em.find(Customer.class, object.getCustomerId());
-    tx.commit();
-    em.close();
-    return objectResult;
-}
+    public static Customer findByDni(String dni) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Customer object = em.find(Customer.class, dni);
+        tx.commit();
+        em.close();
+        return object;
+    }
 
-public static void deleteById(Integer id) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    em.remove(em.find(Customer.class, id));
-    tx.commit();
-    em.close();
-}
+    public static Customer load(String dni) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Customer objectResult = em.find(Customer.class, dni);
+        tx.commit();
+        em.close();
+        return objectResult;
+    }
 
-public static void delete(Customer object) {
-    EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    em.remove(em.find(Object.class, object.getCustomerId()));
-    tx.commit();
-    em.close();
-}
-/* ACTUALMENTE NO SIRVE PERO LO DEJO
+    public static Customer load(Customer object) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Customer objectResult = em.find(Customer.class, object.getCustomerId());
+        tx.commit();
+        em.close();
+        return objectResult;
+    }
+
+    public static void deleteByDni(String dni) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.remove(em.find(Customer.class, dni));
+        tx.commit();
+        em.close();
+    }
+
+    public static void delete(Customer object) {
+        EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.remove(em.find(Object.class, object.getDni()));
+        tx.commit();
+        em.close();
+    }
+    /* ACTUALMENTE NO SIRVE PERO LO DEJO
 public static List<Car> findByName(String name) {
     EntityManager em = PersistenceUtil.getEntityManagerFactory().createEntityManager();
     EntityTransaction tx = em.getTransaction();
@@ -112,5 +105,5 @@ public static List<Car> findByName(String name) {
     em.close();
     return list;
 }
-*/
+     */
 }
