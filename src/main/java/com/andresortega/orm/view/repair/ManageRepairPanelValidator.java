b46be64cfc3eb360021e86d59d0dd72171c16f67
androrho <aortblan@gmail.com>
2025-01-19 08:37:06 +0100

    refactorización

--- src/main/java/com/andresortega/orm/view/repair/ManageRepairPanelValidator.java
+++ src/main/java/com/andresortega/orm/view/repair/ManageRepairPanelValidator.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.andresortega.orm.view.repair;
 
 
 import com.andresortega.controller.RepairService;
 import com.andresortega.model.Repair;
+import static com.andresortega.orm.view.util.Dialog.errorMessage;
 import static com.andresortega.orm.view.util.Dialog.warningMessage;
 
 
@@ -18,16 +15,18 @@ import static com.andresortega.orm.view.util.Dialog.warningMessage;
 public class ManageRepairPanelValidator {
     
     public static boolean areFieldsValid(int selectedIndex){
+        
         if (!isEngineTypeValid(selectedIndex)) {
             return false;
         }
+        
         return true;
     }
     
     public static boolean isEngineTypeValid(int selectedIndex) {
 
         if (selectedIndex == -1) {
-            warningMessage("Selecciona un estado de reparación.", "Error");
+            warningMessage("Selecciona un estado de reparación.", "Advertencia");
             return false;
         }
 
@@ -37,7 +36,7 @@ public class ManageRepairPanelValidator {
     public static boolean isRowSelected(int selectedRow) {
         
         if (selectedRow == -1) {
-            warningMessage("Selecciona una fila en la tabla.", "Error");
+            warningMessage("Selecciona una fila en la tabla.", "Advertencia");
             return false;
         } else {
             return true;
@@ -47,7 +46,7 @@ public class ManageRepairPanelValidator {
     public static boolean isUpdateRestricted(Repair r){
         
         if (repairIsPickedUp(r)) {
-            warningMessage("El coche ya no está en el taller.", "Error");
+            errorMessage("El coche ya no está en el taller.", "Error");
             return true;
         }
         
