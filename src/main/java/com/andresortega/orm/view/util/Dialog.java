a3f73b84edc4bf16cb5649415bbf0b159bc9bba2
androrho <aortblan@gmail.com>
2025-01-19 08:12:11 +0100

    creada clase dialogos

--- src/main/java/com/andresortega/orm/view/util/Dialog.java
+++ src/main/java/com/andresortega/orm/view/util/Dialog.java
@@ -4,10 +4,32 @@
  */
 package com.andresortega.orm.view.util;
 
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Andrés
  */
 public class Dialog {
+    
+    final static String WARNING_IMAGE_PATH = "src/main/resources/images/warning.png";
+    final static String INFO_IMAGE_PATH = "src/main/resources/images/info.png";
+    final static String ERROR_IMAGE_PATH = "src/main/resources/images/error.png";
+    
+    public static void warningMessage(String message, String title) {
+        ImageIcon icon = new ImageIcon(WARNING_IMAGE_PATH);
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.WARNING_MESSAGE, icon);
+    }
+    
+    public static void errorMessage(String message, String title) {
+        ImageIcon icon = new ImageIcon(WARNING_IMAGE_PATH);
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.WARNING_MESSAGE, icon);
+    }
+    
+    public static void infoMessage(String message, String title) {
+        ImageIcon icono = new ImageIcon(INFO_IMAGE_PATH);
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE, icono);
+    }
     
 }
