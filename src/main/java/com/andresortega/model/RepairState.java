3491fdcac6fc2275a95106e18c0ead2bd9417f8e
androrho <aortblan@gmail.com>
2025-01-19 08:43:06 +0100

    refactorización

--- src/main/java/com/andresortega/model/RepairState.java
+++ src/main/java/com/andresortega/model/RepairState.java
@@ -5,23 +5,9 @@ package com.andresortega.model;
  * @author Andrés
  */
 public enum RepairState {
-    /**
-     * The singleton instance for the day-of-week of Tuesday. This has the
-     * numeric value of {@code 2}.
-     */
     PENDING,
-    /**
-     * The singleton instance for the day-of-week of Wednesday. This has the
-     * numeric value of {@code 3}.
-     */
     IN_PROCESS,
-    /**
-     * The singleton instance for the day-of-week of Thursday. This has the
-     * numeric value of {@code 4}.
-     */
     COMPLETED,
-    
     PICK_UP,
-    
     PICKED_UP;
 }
