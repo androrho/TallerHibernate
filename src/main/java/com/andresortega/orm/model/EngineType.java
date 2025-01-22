05a244a0411ae330d7114e412505a161e680fcf0
androrho <aortblan@gmail.com>
2025-01-22 11:18:25 +0100

    nombre cambiado

--- src/main/java/com/andresortega/orm/model/EngineType.java
+++ src/main/java/com/andresortega/orm/model/EngineType.java
@@ -5,24 +5,8 @@ package com.andresortega.orm.model;
  * @author Andrés
  */
 public enum EngineType {
-    /**
-     * The singleton instance for the day-of-week of Monday. This has the
-     * numeric value of {@code 1}.
-     */
-    PETROL,
-    /**
-     * The singleton instance for the day-of-week of Tuesday. This has the
-     * numeric value of {@code 2}.
-     */
+    GASOLINA,
     DIESEL,
-    /**
-     * The singleton instance for the day-of-week of Wednesday. This has the
-     * numeric value of {@code 3}.
-     */
-    ELECTRIC,
-    /**
-     * The singleton instance for the day-of-week of Thursday. This has the
-     * numeric value of {@code 4}.
-     */
-    HYBRID;
+    ELECTRICO,
+    HIBRIDO;
 }
